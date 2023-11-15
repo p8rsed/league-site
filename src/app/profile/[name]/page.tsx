811d1b4f0e457db1aa0bd40b70156bc827242f9f
p8rsed <150688538+p8rsed@users.noreply.github.com>
2023-11-15 15:41:36 +0000

    now showing more info on matches

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -1,9 +1,16 @@
-import { fetchProfileMatches, getSummonerByName } from "@/lib/riot-api/api";
-import { LeagueMatch } from "@/lib/riot-api/types/league-match";
+import {
+  fetchProfileMatches,
+  formatChampionSquareAsset,
+  formatItemSquareAsset,
+  formatSummonerSpellSquareAsset,
+  getSummonerByName,
+} from "@/lib/riot-api/api";
+import { LeagueMatch, Participant } from "@/lib/riot-api/types/league-match";
 import { Profile } from "@/lib/riot-api/types/profile";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { useId } from "react";
 dayjs.extend(relativeTime);
 
 export const revalidate = 3600; // revalidate at most every hour
@@ -68,31 +75,198 @@ function ProfileHeader({ profile }: { profile: Profile }) {
   );
 }
 
+function Pings({ participant }: { participant: Participant }) {
+  const allInPings = participant.allInPings;
+  const assistMePings = participant.assistMePings;
+  const baitPings = participant.baitPings;
+  const basicPings = participant.basicPings;
+  const commandPings = participant.commandPings;
+  const dangerPings = participant.dangerPings;
+  const enemyMissingPings = participant.enemyMissingPings;
+  const enemyVisionPings = participant.enemyVisionPings;
+  const getBackPings = participant.getBackPings;
+  const holdPings = participant.holdPings;
+  const needVisionPings = participant.needVisionPings;
+  const onMyWayPings = participant.onMyWayPings;
+  const pushPings = participant.pushPings;
+  const visionClearedPings = participant.visionClearedPings;
+
+  const pings = {
+    allInPings,
+    assistMePings,
+    baitPings,
+    basicPings,
+    commandPings,
+    dangerPings,
+    enemyMissingPings,
+    enemyVisionPings,
+    getBackPings,
+    holdPings,
+    needVisionPings,
+    onMyWayPings,
+    pushPings,
+    visionClearedPings,
+  };
+
+  return (
+    <div>
+      {Object.entries(pings).map(([key, value]) => {
+        return (
+          <div key={`ping-${key}`}>
+            {key}: {value}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 function Match({ profile, match }: { profile: Profile; match: LeagueMatch }) {
   const participant = match.info.participants.find(
     (p) => p.puuid === profile.puuid
   )!;
   const outcome = participant.win ? "Victory" : "Defeat";
-  // Match date
-  const date = new Date(match.info.gameCreation);
-  const champion = participant.championName;
+  const matchDate = new Date(match.info.gameCreation);
+  const championName = participant.championName;
+  // Get build items
 
   return (
     <div
       key={match.metadata.matchId}
-      className={`rounded flex items-center border border-neutral-500 p-2 ${
+      className={`space-x-4 rounded-md flex items-center border border-neutral-500 p-2 ${
         outcome === "Victory" ? "bg-[#28344E]" : "bg-[#59343B]"
       }`}
     >
-      <div className="flex items-center gap-x-2">
-        <div className="flex flex-col text-center">
+      <div className="flex items-center gap-x-2 w-24">
+        <div className="flex flex-col text-center text-sm">
           <span>{match.info.gameMode}</span>
-          <span>{dayjs(date).fromNow()}</span>
-          <span>{outcome}</span>
-          <span>{Number(match.info.gameDuration / 60).toFixed(2)} minutes</span>
+          <span>{dayjs(matchDate).fromNow()}</span>
+          <span className="font-bold">{outcome}</span>
+          <span>{Number(match.info.gameDuration / 60).toFixed(0)} minutes</span>
         </div>
       </div>
-      <div className="flex-1 flex justify-center">{champion}</div>
+      <div className="flex-1 flex justify-start items-center space-x-2">
+        <div className="overflow-hidden rounded-full">
+          <Image
+            src={formatChampionSquareAsset({
+              championName,
+            })}
+            alt={championName}
+            height={64}
+            width={64}
+          />
+        </div>
+        <ParticipantSummonerSpells participant={participant} />
+        <ParticipantStats participant={participant} match={match} />
+        <div className="pl-2">
+          <ParticipantItems participant={participant} />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+function ParticipantItems({ participant }: { participant: Participant }) {
+  const id = useId();
+
+  const items = [
+    participant.item0,
+    participant.item1,
+    participant.item2,
+    participant.item3,
+    participant.item4,
+    participant.item5,
+  ];
+
+  const trinket = participant.item6;
+
+  return (
+    <div className="flex">
+      <div className="grid grid-cols-3">
+        {items
+          .filter((item) => item) // 0 is empty
+          .map((item) => (
+            <div key={`${id}-${item}`}>
+              <Image
+                src={formatItemSquareAsset({ itemId: item })}
+                alt={`${item}`}
+                height={24}
+                width={24}
+              />
+            </div>
+          ))}
+      </div>
+      <div>
+        <Image
+          src={formatItemSquareAsset({ itemId: trinket })}
+          alt={`trinket-${trinket}`}
+          height={24}
+          width={24}
+        />
+      </div>
+    </div>
+  );
+}
+
+export function ParticipantSummonerSpells({
+  participant,
+}: {
+  participant: Participant;
+}) {
+  const id = useId();
+  const summoner1 = participant.summoner1Id;
+  const summoner2 = participant.summoner2Id;
+  const spells = [summoner1, summoner2];
+  return (
+    <div className="grid grid-cols-1">
+      {spells.map((spell) => (
+        <div key={`${id}-${spell}`}>
+          <Image
+            src={formatSummonerSpellSquareAsset({ spellId: spell })}
+            alt={`summoner spell ${spell}`}
+            height={24}
+            width={24}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function ParticipantStats({
+  participant,
+  match,
+}: {
+  participant: Participant;
+  match: LeagueMatch;
+}) {
+  const k = participant.kills;
+  const d = participant.deaths;
+  const a = participant.assists;
+  const cs = participant.totalMinionsKilled + participant.neutralMinionsKilled;
+  const gameDuration = match.info.gameDuration / 60;
+  const csPerMin = cs / gameDuration;
+
+  const sepClass = "text-neutral-400 px-0.5 font-semibold";
+
+  return (
+    <div className="w-24 flex flex-col justify-center items-center space-y-1">
+      <h1 className="">
+        <span>{k}</span>
+        <span className={sepClass}>/</span>
+        <span>{d}</span>
+        <span className={sepClass}>/</span>
+        <span>{a}</span>
+      </h1>
+      <div className="">
+        <h2 className="text-xs space-x-1">
+          <span>
+            <span>{cs}</span> <span className="">CS</span>
+          </span>
+          <span>{"(" + csPerMin.toFixed(1) + ")"}</span>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/lib/riot-api/api.ts
+++ src/lib/riot-api/api.ts
@@ -1,3 +1,4 @@
+import { SUMMONER_SPELLS } from "@/lib/riot-api/data/summoner-spells";
 import { LeagueMatch } from "@/lib/riot-api/types/league-match";
 
 const key = process.env.API_KEY!;
@@ -51,4 +52,34 @@ export async function getSummonerByName({ name }: { name: string }) {
   );
 
   return response.json();
+}
+
+// Assets
+export function formatChampionSquareAsset({
+  championName,
+}: {
+  championName: string;
+}) {
+  // make first letter capital because riot api is lol
+  const name = championName.charAt(0).toUpperCase() + championName.slice(1);
+  return `https://ddragon.leagueoflegends.com/cdn/13.22.1/img/champion/${name}.png`;
+}
+
+export function formatItemSquareAsset({ itemId }: { itemId: number }) {
+  return `https://ddragon.leagueoflegends.com/cdn/13.22.1/img/item/${itemId}.png`;
+}
+
+export function formatSummonerSpellSquareAsset({
+  spellId,
+}: {
+  spellId: number;
+}) {
+  const match = Object.entries(SUMMONER_SPELLS.data).find(([key, value]) => {
+    if (value.key == String(spellId)) {
+      return true;
+    }
+  });
+
+  const id = match?.[1].id;
+  return `https://ddragon.leagueoflegends.com/cdn/13.22.1/img/spell/${id}.png`;
 }
